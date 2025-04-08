94cbc6d33aa0a0af3834f7c72eb42bba2a357615
Furkan Özel <work.kan314@gmail.com>
2025-04-08 12:10:47 +0200

    feat: add go program that upates a bpf map

---
+++ updater/main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/cilium/ebpf"
+)
+
+const (
+	map_key uint32 = 4
+)
+
+func main() {
+	// Path where the eBPF map is pinned
+	mapPath := "/sys/fs/bpf/kernel_function/map_policy"
+
+	// Open the pinned map
+	pinnedMap, err := ebpf.LoadPinnedMap(mapPath, nil)
+	if err != nil {
+		log.Fatalf("Error opening pinned map: %v", err)
+	}
+
+	valueStr := "Hello eBPF!"
+	var mapValue [64]byte
+	copy(mapValue[:], valueStr)
+
+	// Update the map
+	if err := pinnedMap.Update(map_key, mapValue, ebpf.UpdateAny); err != nil {
+		log.Fatalf("Error updating map: %v", err)
+	}
+	log.Printf("Successfully wrote value %s to map key ", mapValue)
+
+	fmt.Println("Map updated successfully")
+}
