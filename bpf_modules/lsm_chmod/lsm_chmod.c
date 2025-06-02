a7d540ec41557c9dae1d4eb53f17838abf58372a
Furkan Özel <work.kan314@gmail.com>
2025-06-02 17:27:37 +0200

    feat(lsm_chmod): add loop that iterates through all cgroup ids

--- bpf_modules/lsm_chmod/lsm_chmod.c
+++ bpf_modules/lsm_chmod/lsm_chmod.c
@@ -1,67 +1,43 @@
-
 //go:build ignore
 
-/*
- * add kernel type definitions (e.g. path->dentry->d_parent->d_name.name,
- * which is the name of the directory in the current path)
- * how to get this file: bpftool btf dump file /sys/kernel/btf/vmlinux format c
- * > vmlinux.h
- */
 #include "../include_dir/vmlinux.h"
-// add bpf helper functions (e.g bpf_map_lookup_elem(), bpf_map_update_elem())
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <linux/errno.h>
-// #include <stdint.h>
 
 char _license[] SEC("license") = "GPL";
 
 struct {
-  // declare pointer called 'type' that points to a int array of the size
-  // 'BPF_MAP_TYPE_ARRAY' (2)
-  __uint(type, BPF_MAP_TYPE_ARRAY);
-  // declare pointer called 'key' that is of the type '__u32'
+  __uint(type, BPF_MAP_TYPE_HASH);
   __type(key, __u32);
-  // declare pointer called 'value' that is of the type '__u64'
   __type(value, __u64);
-  // declare pointer called 'max_entries' that points to a int array of the size
-  // 5
-  __uint(max_entries, 5);
+  // max 64 docker containers can be observed
+  __uint(max_entries, 64);
   __uint(pinning, LIBBPF_PIN_BY_NAME);
 } map_policy SEC(".maps");
 
-/*
- * available LSM hooks: https://www.kernel.org/doc/html/v5.2/security/LSM.html
- * how to get BPF_PROG function declaration for the path_chmod LSM hook:
- * grep path_chmod oth/X/kernel/linux-6.10/include/linux/lsm_hook_defs.h
- */
 SEC("lsm/path_chmod")
-/*
- * this program executes whenever a chmod command is performed
- * it reads the name of the directory in which the command is performed
- * and compares it to a given string, "furkan" in this case
- * if it is the same, chmod will not be executed
- * the path argument is the kernel data structure representing the file
- * the mode argument is the desired new mode value
- */
-
 int BPF_PROG(path_chmod, const struct path *path, umode_t mode) {
-  int key = 0;
-  __u64 *value = bpf_map_lookup_elem(&map_policy, &key);
-  if (value)
-    bpf_printk("Value read from the map: '%llu'\n", *value);
-  else
-    bpf_printk("Failed to read value from the map\n");
-
-  ///
+  __u32 key;
+  __u64 *value;
   __u64 cgrp_id = bpf_get_current_cgroup_id();
-  bpf_printk("lets gooo cgroup_id: %llu\n", cgrp_id);
-  if (cgrp_id == *value) {
-    bpf_printk("lets gooo cgroup_id: %llu\n", cgrp_id);
-    bpf_printk("chmod not allowed");
-    return -EPERM;
+  bpf_printk("current cgroup_id: %llu\n", cgrp_id);
+
+// loop through all cgroup ids
+#pragma unroll
+  for (int i = 0; i < 64; i++) {
+    key = i;
+    value = bpf_map_lookup_elem(&map_policy, &key);
+    if (!value)
+      continue;
+
+    if (cgrp_id == *value) {
+      bpf_printk("matched cgroup_id %llu in map at key %d → blocking chmod\n",
+                 cgrp_id, i);
+      return -EPERM;
+    }
   }
-  ///
-  // return -EPERM;
+
+  // else allow chmod
   return 0;
 }
